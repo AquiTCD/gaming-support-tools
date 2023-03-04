37449ae7ea466663e3cef26649dbcf3c8c96f761
Aqui TSUCHIDA <aqui.tcd+dev@gmail.com>
2023-03-04 17:41:38 +0900

    chore: apply tailwind as sample

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -6,7 +6,7 @@ export default function Title({ name }: Props): JSX.Element {
   return (
     <>
       <h1>Hello from React Component</h1>
-      <h3>Hello {name ?? "world"}</h3>
+      <h3 className="font-bold">Hello {name ?? "world"}</h3>
     </>
   );
 }
