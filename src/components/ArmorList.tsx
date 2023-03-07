66e444e506eee1242f6191ec9b985a1e38f9ca89
Aqui TSUCHIDA <aqui.tcd+dev@gmail.com>
2023-03-08 02:08:37 +0900

    separate filter

--- src/components/ArmorList.tsx
+++ src/components/ArmorList.tsx
@@ -9,7 +9,6 @@ export default function ArmorList(): JSX.Element {
   const $armorList = useStore(armorList)
   const $positionFilter = useStore(positionFilter)
   const $skillFilter = useStore(skillFilter)
-  const allSkills = new Set($armorList.flatMap(armor => armor.skills ))
 
   const filteredArmorList = () => {
     let list = $armorList
@@ -22,46 +21,8 @@ export default function ArmorList(): JSX.Element {
     return list
   }
 
-  const positionButtonColorClass = {
-    head: 'bg-orange-300',
-    body: 'bg-lime-300',
-    arm: 'bg-emerald-300',
-    waist: 'bg-cyan-300',
-    leg: 'bg-indigo-300',
-  }
-
   return (
     <>
-      <h2 className="flex flex-row flex-nowrap items-center my-8">
-        <span className="flex-grow block border-t border-black" aria-hidden="true" role="presentation"></span>
-        <span className="flex-none block mx-4 px-4 py-2.5 text-xs leading-none font-medium uppercase bg-black text-white">
-          装備リスト
-        </span>
-        <span className="flex-grow block border-t border-black" aria-hidden="true" role="presentation"></span>
-      </h2>
-
-      <div className="px-5 py-3 border-gray-500 border">
-        <span>絞りこみ: </span>
-        { positions.map((position, i) => {
-          const colorClasses = $positionFilter.includes(position) ? `${positionButtonColorClass[position]} text-gray-700 font-bold` : "bg-gray-200 text-gray-500"
-          const classes = `rounded-full text-sm px-4 py-1 mr-2 ${colorClasses}`
-          return <button key={i}
-            className={classes}
-            onClick={() => togglePositionFilter(position)}>{i18nPosition[position]}</button>
-          })
-        }
-        <br />
-        { Array.from(allSkills).map((skill, i) => {
-          const colorClasses = $skillFilter.includes(skill) ? "bg-pink-200 text-gray-700 font-bold" : "bg-gray-200 text-gray-500"
-          const classes = `rounded-full text-sm px-4 py-1 mr-2 ${colorClasses}`
-          return <button key={i}
-            className={classes}
-            onClick={() => toggleSkillFilter(skill)}>{skill}</button>
-          })
-        }
-      </div>
-      <hr />
-
       <table className="relative min-w-max w-full table-auto">
         <thead>
           <tr className="text-gray-600 uppercase text-sm leading-normal">
