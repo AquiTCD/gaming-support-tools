d43ae18c9f53c2bdb77de9a51b1c2a4cbe545d84
Aqui TSUCHIDA <aqui.tcd+dev@gmail.com>
2023-03-08 13:09:46 +0900

    setup modal

--- src/components/Filter.tsx
+++ src/components/Filter.tsx
@@ -1,5 +1,7 @@
 import { useStore } from '@nanostores/react'
+import React, { useState } from 'react'
 import { positions, positionFilter, armorList, equip, togglePositionFilter, skillFilter, toggleSkillFilter } from '@/stores/armor-sim'
+import Modal from '@/components/Modal'
 import { i18nPosition } from '@/utils/utils'
 import type { Loadout, Position, Armor } from '@/types/types'
 
@@ -8,6 +10,7 @@ export default function Filter(): JSX.Element {
   const $positionFilter = useStore(positionFilter)
   const $skillFilter = useStore(skillFilter)
   const allSkills = new Set($armorList.flatMap(armor => armor.skills ))
+  const [show, setShow] = useState(false)
 
   const positionButtonColorClass = {
     head: 'bg-orange-300',
@@ -44,6 +47,10 @@ export default function Filter(): JSX.Element {
             })
           }
         </div>
+        <button onClick={() => setShow(true)} data-modal-target="defaultModal" data-modal-toggle="defaultModal" className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">
+          Toggle modal
+        </button>
+        <Modal show={show} setShow={setShow} />
       </div>
     </>
   );
