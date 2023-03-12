cb3d714ee9e62270124b07a5b30261635ef2ce5c
Aqui TSUCHIDA <aqui.tcd+dev@gmail.com>
2023-03-13 08:22:17 +0900

    fix lock button style

--- src/components/Loadout.tsx
+++ src/components/Loadout.tsx
@@ -117,7 +117,7 @@ export default function Loadout(): JSX.Element {
     const isLocked = $currentLoadout[armor.position]?.isLocked ?? false
     const svg = <svg fill="none" stroke="currentColor" strokeWidth={2.5} viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">{ isLocked ? closeImg : openImg }</svg>
     return (
-      <button className="w-4 h-4 pt-1" onClick={() => toggleLock(armor.position)}>{svg}</button>
+      <button className="w-4 pt-1" onClick={() => toggleLock(armor.position)}>{svg}</button>
     )
   }
 
