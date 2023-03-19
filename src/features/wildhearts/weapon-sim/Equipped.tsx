3b0247802055b6c32367d4cd45617ecc47b31ddb
Aqui TSUCHIDA <aqui.tcd+dev@gmail.com>
2023-03-19 17:41:02 +0900

    add draggable

--- src/features/wildhearts/weapon-sim/Equipped.tsx
+++ src/features/wildhearts/weapon-sim/Equipped.tsx
@@ -2,6 +2,7 @@ import { useStore } from '@nanostores/react'
 import { selection, allWeapons } from '@/stores/wildhearts/weapon-sim'
 import type { Weapon, Select, InheritedSkill } from '@/types/wildhearts/weapon'
 import { useEffect, useState } from "react"
+import Draggable, {DraggableCore} from 'react-draggable'
 
 type Props={
   name?: string;
@@ -12,67 +13,66 @@ export default function Equipped({ name }: Props): JSX.Element {
   const lastSelected = $selection[$selection.length - 1]
   const EquippedWeapon: Weapon | undefined = allWeapons.find(weapon => weapon.coord === lastSelected.coord)
   const inheritedSkills: InheritedSkill[] = lastSelected.skills
-  const [isClient, setIsClient] = useState(false)
-  useEffect(() => {
-    setIsClient(true);
-  }, []);
 
   return (
     <>
-    {isClient &&
-      <table className="border-separate border-4 border-gray-700 w-80 rounded-lg border-spacing-2">
-        <tbody>
-        <tr>
-          <td className="border-b-2 border-gray-700 text-center text-lg md:text-xl py-1 md:py-2" colSpan={2}>{EquippedWeapon?.name}</td>
-        </tr>
-        <tr>
-          <th className="border-b-2 border-r border-gray-700">攻撃力</th>
-          <td className="border-b-2 border-gray-700">
-            <span>{EquippedWeapon?.charac}</span>
-            <span>{EquippedWeapon?.attack}</span>
-          </td>
-        </tr>
-        <tr>
-          <th className="border-b-2 border-r border-gray-700">属性攻撃力</th>
-          <td className="border-b-2 border-gray-700">
-            <span>{EquippedWeapon?.attribute}</span>
-            <span>{EquippedWeapon?.attributePower}</span>
-          </td>
-        </tr>
-        <tr>
-          <th className="border-b-2 border-r border-gray-700">会心率</th>
-          <td className="border-b-2 border-gray-700">
-            <span>{EquippedWeapon?.critical}</span>
-            <span>%</span>
-          </td>
-        </tr>
-        <tr>
-          <th className="border-b-2 border-gray-700" colSpan={2}>固有技能</th>
-        </tr>
-        <tr>
-          <td colSpan={2}>
-            <ul>
-              { [0,1,2].map(i => {
-                return <li key={i} className="border-b border-gray-700 h-6">{EquippedWeapon?.inherentSkills[i]}</li>
-              })}
-            </ul>
-          </td>
-        </tr>
-        <tr>
-          <th className="border-b-2 border-gray-700" colSpan={2}>継承技能</th>
-        </tr>
-        <tr>
-          <td colSpan={2}>
-            <ul>
-              { [0,1,2,3,4].map(i => {
-                return <li key={i} className="border-b border-gray-700 h-6">{inheritedSkills[i]?.name}</li>
-              })}
-            </ul>
-          </td>
-        </tr>
-        </tbody>
-      </table>
-      }
+      <Draggable
+        handle="#equipped"
+        defaultPosition={{x: 0, y: 0}}
+      >
+        <table id="equipped" className="bg-gray-800/75 border-separate border-4 border-amber-400 text-gray-100 w-80 rounded-lg border-spacing-2 handle absolute bottom-10 right-10 cursor-grab active:cursor-grabbing">
+          <tbody>
+          <tr>
+            <td className="border-b-2 border-amber-200 text-center text-lg md:text-xl py-1 md:py-2 font-bold" colSpan={2}>{EquippedWeapon?.name}</td>
+          </tr>
+          <tr>
+            <th className="border-b-2 border-amber-200 text-right font-normal w-28">攻撃力</th>
+            <td className="border-b-2 border-amber-200 text-right pr-10 font-bold text-l">
+              <span className="mr-2">{EquippedWeapon?.charac}</span>
+              <span>{EquippedWeapon?.attack}</span>
+            </td>
+          </tr>
+          <tr>
+            <th className="border-b-2 border-amber-200 text-right font-normal w-28">属性攻撃力</th>
+            <td className="border-b-2 border-amber-200 text-right pr-10 font-bold text-l">
+              <span className="mr-2">{EquippedWeapon?.attribute}</span>
+              <span>{EquippedWeapon?.attributePower}</span>
+            </td>
+          </tr>
+          <tr>
+            <th className="border-b-2 border-amber-200 text-right font-normal w-28">会心率</th>
+            <td className="border-b-2 border-amber-200 text-right pr-10 font-bold text-l">
+              <span>{EquippedWeapon?.critical}</span>
+              <span>%</span>
+            </td>
+          </tr>
+          <tr>
+            <th className="border-b-2 border-amber-200" colSpan={2}>固有技能</th>
+          </tr>
+          <tr>
+            <td colSpan={2}>
+              <ul>
+                { [0,1,2].map(i => {
+                  return <li key={i} className="bg-gray-900/75 h-7 pt-0.5 mb-1 px-2">{EquippedWeapon?.inherentSkills[i]}</li>
+                })}
+              </ul>
+            </td>
+          </tr>
+          <tr>
+            <th className="border-b-2 border-amber-200" colSpan={2}>継承技能</th>
+          </tr>
+          <tr>
+            <td colSpan={2}>
+              <ul>
+                { [0,1,2,3,4].map(i => {
+                  return <li key={i} className="bg-gray-900/75 h-7 pt-0.5 mb-1 px-2">{inheritedSkills[i]?.name}</li>
+                })}
+              </ul>
+            </td>
+          </tr>
+          </tbody>
+        </table>
+      </Draggable>
     </>
   )
 }
