9b75756f96dde2afb4b75c85d27220f5221cff48
Aqui TSUCHIDA <aqui.tcd+dev@gmail.com>
2023-04-01 16:25:40 +0900

    skill modal

--- src/features/wildhearts/weapon-sim/components/SelectSkillModal.tsx
+++ src/features/wildhearts/weapon-sim/components/SelectSkillModal.tsx
@@ -1,16 +1,26 @@
 import { useStore } from '@nanostores/react'
 import { modalStates, close } from '@/features/wildhearts/weapon-sim/stores/modals'
-import { inherentSkills, inheritedSkills, setCandidate } from '@/features/wildhearts/weapon-sim/stores/skills'
+import { inherentSkills, inheritedSkills, candidateSkills, setCandidate, removeCandidate } from '@/features/wildhearts/weapon-sim/stores/skills'
 import type { Candidate } from '@/features/wildhearts/weapon-sim/models/skill'
 
 export default function SelectSkillModal(): JSX.Element | null {
   const $inherentSkills = useStore(inherentSkills)
   const $inheritedSkills = useStore(inheritedSkills)
   const $modalStates = useStore(modalStates)
-  console.log($modalStates)
+  const $candidateSkills = useStore(candidateSkills)
   const candidate = $modalStates.selectSkillModal as Candidate
 
   const skills = candidate === 'inherent' ? $inherentSkills : $inheritedSkills
+  const bgColor = candidate === 'inherent' ? "bg-teal-200" : "bg-fuchsia-200"
+  const currentSkill = $candidateSkills[candidate]
+  const clickHandler = (skill) => {
+    if (currentSkill === skill) {
+      removeCandidate(candidate)
+    } else {
+      setCandidate(candidate, skill)
+      close('selectSkillModal')
+    }
+  }
 
   if (Boolean(candidate)) {
     return (
@@ -18,18 +28,16 @@ export default function SelectSkillModal(): JSX.Element | null {
         <div className="fixed text-xs md:text-sm bg-gray-600 bg-opacity-50 top-0 left-0 right-0 z-50 w-full p-10 overflow-x-hidden overflow-y-auto md:inset-0 h-[calc(100%-1rem)] md:h-full">
           <div className="relative w-full h-full md:h-auto">
             <div className="relative bg-gray-800 rounded-lg shadow border-4 border-amber-400">
-              <div className="px-4 py-2 space-y-2 grid justify-items-center text-xs md:text-sm text-gray-200">
-                <ul>
-                  { skills.map((skill, i) => {
-                    return (
-                      <li key={i}>
-                        <button onClick={() => {setCandidate(candidate, skill); close('selectSkillModal')}}>
-                          {skill.name}
-                        </button>
-                      </li>
-                    )
-                  })}
-                </ul>
+              <div className="px-4 py-2 space-y-2 text-xs md:text-sm text-gray-200">
+                { skills.map((skill, i) => {
+                    const colorClasses = currentSkill === skill ? `${bgColor} text-gray-900 font-bold` : "bg-gray-300 text-gray-700"
+                    const classes = `rounded-full px-2 md:px-3 py-1 mr-2 ${colorClasses}`
+                    return <button key={i}
+                      className={classes}
+                      onClick={() => clickHandler(skill)}
+                      data-tooltip-id="skill-tooltip" data-tooltip-content={skill.name}>{skill.name}</button>
+                    })
+                }
               </div>
               <div className="flex justify-end items-center px-3 py-2 space-x-2">
                 <button type="button" onClick={() => close('selectSkillModal')} className="text-amber-100 bg-gray-800 border-2 border-amber-400 hover:bg-amber-600 rounded-lg text-sm px-3 py-2 text-center">キャンセル</button>
--- src/features/wildhearts/weapon-sim/components/SkillModal.tsx
+++ src/features/wildhearts/weapon-sim/components/SkillModal.tsx
@@ -1,9 +1,22 @@
 import { useStore } from '@nanostores/react'
 import { modalStates, open, close } from '@/features/wildhearts/weapon-sim/stores/modals'
-import { inherentSkills, inheritedSkills } from '@/features/wildhearts/weapon-sim/stores/skills'
+import { candidateSkills } from '@/features/wildhearts/weapon-sim/stores/skills'
 
 export default function SkillModal(): JSX.Element | null {
   const $modalStates = useStore(modalStates)
+  const $candidateSkills = useStore(candidateSkills)
+
+  const buttonColor = (candidate) => {
+    if (Boolean($candidateSkills[candidate])) {
+      return candidate === 'inherent' ? "bg-teal-200 text-gray-900 font-bold" : "bg-fuchsia-200  text-gray-900 font-bold"
+    } else {
+      return "bg-gray-300 text-gray-700"
+    }
+  }
+  const buttonText = (candidate) => {
+    return $candidateSkills[candidate]?.name ?? "設定する"
+  }
+
   const isOpen = $modalStates.skillModal
 
   if (isOpen) {
@@ -13,41 +26,73 @@ export default function SkillModal(): JSX.Element | null {
           <div className="relative w-full h-full md:h-auto">
             <div className="relative bg-gray-800 rounded-lg shadow border-4 border-amber-400">
               <div className="px-4 py-2 space-y-2 grid justify-items-center text-xs md:text-sm text-gray-200">
-                <ul>
-                  <li>
-                    <button onClick={() => open('selectSkillModal', 'inherent')}>
-                      固有技能 候補
-                    </button>
-                  </li>
-                  <li>
-                    <button onClick={() => open('selectSkillModal', 'inherited1')}>
-                      継承技能 候補1
-                    </button>
-                  </li>
-                  <li>
-                    <button onClick={() => open('selectSkillModal', 'inherited2')}>
-                      継承技能 候補2
-                    </button>
-                  </li>
-                  <li>
-                    <button onClick={() => open('selectSkillModal', 'inherited3')}>
-                      継承技能 候補3
-                    </button>
-                  </li>
-                  <li>
-                    <button onClick={() => open('selectSkillModal', 'inherited4')}>
-                      継承技能 候補4
-                    </button>
-                  </li>
-                  <li>
-                    <button onClick={() => open('selectSkillModal', 'inherited5')}>
-                      継承技能 候補5
-                    </button>
-                  </li>
-                </ul>
+                <table>
+                  <tbody>
+                    <tr>
+                      <th className="text-sm md:text-base">固有技能 候補</th>
+                      <td className="py-2 px-2">
+                        <button onClick={() => open('selectSkillModal', 'inherent')}
+                          className={`${buttonColor('inherent')} rounded-full px-2 md:px-3 py-1 mr-2 `}
+                        >
+                          {buttonText('inherent')}
+                        </button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th className="text-sm md:text-base">継承技能 候補1</th>
+                      <td className="py-2 px-2">
+                        <button onClick={() => open('selectSkillModal', 'inherited1')}
+                          className={`${buttonColor('inherited1')} rounded-full px-2 md:px-3 py-1 mr-2 `}
+                        >
+                          {buttonText('inherited1')}
+                        </button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th className="text-sm md:text-base">継承技能 候補2</th>
+                      <td className="py-2 px-2">
+                        <button onClick={() => open('selectSkillModal', 'inherited2')}
+                          className={`${buttonColor('inherited2')} rounded-full px-2 md:px-3 py-1 mr-2 `}
+                        >
+                          {buttonText('inherited2')}
+                        </button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th className="text-sm md:text-base">継承技能 候補3</th>
+                      <td className="py-2 px-2">
+                        <button onClick={() => open('selectSkillModal', 'inherited3')}
+                          className={`${buttonColor('inherited3')} rounded-full px-2 md:px-3 py-1 mr-2 `}
+                        >
+                          {buttonText('inherited3')}
+                        </button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th className="text-sm md:text-base">継承技能 候補4</th>
+                      <td className="py-2 px-2">
+                        <button onClick={() => open('selectSkillModal', 'inherited4')}
+                          className={`${buttonColor('inherited4')} rounded-full px-2 md:px-3 py-1 mr-2 `}
+                        >
+                          {buttonText('inherited4')}
+                        </button>
+                      </td>
+                    </tr>
+                    <tr>
+                      <th className="text-sm md:text-base">継承技能 候補5</th>
+                      <td className="py-2 px-2">
+                        <button onClick={() => open('selectSkillModal', 'inherited5')}
+                          className={`${buttonColor('inherited5')} rounded-full px-2 md:px-3 py-1 mr-2 `}
+                        >
+                          {buttonText('inherited5')}
+                        </button>
+                      </td>
+                    </tr>
+                  </tbody>
+                </table>
               </div>
               <div className="flex justify-end items-center px-3 py-2 space-x-2">
-                <button type="button" onClick={() => close('skillModal')} className="text-amber-100 bg-gray-800 border-2 border-amber-400 hover:bg-amber-600 rounded-lg text-sm px-3 py-2 text-center">キャンセル</button>
+                <button type="button" onClick={() => close('skillModal')} className="text-amber-100 bg-gray-800 border-2 border-amber-400 hover:bg-amber-600 rounded-lg text-sm px-3 py-2 text-center">OK</button>
               </div>
             </div>
           </div>
